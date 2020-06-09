374a68114cd9ffeb54ed5482968502ea55420b3c
Gentoku Takano <gentoku.nika@gmail.com>
2020-06-09 18:19:37 +0900

    user/index 完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,6 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  def after_sign_in_path_for(resource)
-    user_path(current_user)
-  end
-
-  def after_sign_up_path_for(resource)
-    user_path(current_user)
-  end
-
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
   end
