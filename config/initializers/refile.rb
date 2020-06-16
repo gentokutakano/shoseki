7809928c7cdc26514880bbd12acda35a5c28eff6
Gentoku Takano <gentoku.nika@gmail.com>
2020-06-16 23:38:36 +0900

    refile-s3

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -3,8 +3,8 @@ require "refile/s3"
 aws = {
   access_key_id: "AWS_ACCESS_KEY_ID",
   secret_access_key: "AWS_SECRET_ACCESS_KEY",
-  region: "リージョン",
-  bucket: "バケット名",
+  region: "ap-northeast-1",
+  bucket: "shoseki-app",
 }
 Refile.cache = Refile::S3.new(prefix: "cache", **aws)
 Refile.store = Refile::S3.new(prefix: "store", **aws)
