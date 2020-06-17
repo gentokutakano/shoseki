6f863e04308b6a193f0247f4cda131645cdcc509
Gentoku Takano <gentoku.nika@gmail.com>
2020-06-17 19:40:35 +0900

    ゲストユーザーログイン機能追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def new_guest
+    user = User.find_or_create_by(email: "guest@example.com", username: "ゲスト") do |user|
+      user.password = SecureRandom.urlsafe_base64
+    end
+    sign_in user
+    redirect_to books_path, notice: "ゲストユーザーでログインしました。"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
+  post "/homes/guest_sign_in", to: "home#new_guest"
   resources :users
   resources :books do
     resource :favorites, only: [:create, :destroy]
